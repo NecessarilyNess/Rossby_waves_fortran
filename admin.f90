6b78400670be14ad7ac7219a6116f8f192813579
Vanessa Madu <vanessamadu197@gmail.com>
2022-11-06 17:24:22 +0000

    formatting

--- admin.f90
+++ admin.f90
@@ -7,14 +7,14 @@ module admin
         real :: start_point, end_point, increment, new_point
         integer :: num_points, counter
         real, dimension(:), allocatable :: points_array
-
+        
         allocate(points_array(num_points))
-        increment = (end_point - start_point)/num_points
+        increment = (end_point - start_point)/(num_points-1)
         new_point = start_point
-
         do counter = 1, num_points
             points_array(counter) = new_point
             new_point = new_point + increment
         end do
+
     end function linspace
 end module admin
