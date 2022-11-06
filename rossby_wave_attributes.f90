66ecc281eb6d3d06d2409be427407f9117913b3b
malawrence1020 <Matthew.X.Lawrence@protonmail.ch>
2022-11-06 17:27:37 +0000

    Amplitude and dispersion

--- rossby_wave_attributes.f90
+++ rossby_wave_attributes.f90
@@ -3,17 +3,17 @@ use constants
 implicit none
 
 contains
-    function amplitude_func(k, l) result(amp)
+    function amplitude(k, l) result(amp)
         real :: k, l, amp
 
         amp = exp(-k**2 - l**2) * (k**2 + l**2)
     
-    end function amplitude_func
+    end function amplitude
 
-    function dispersion_func(k, l) result(omega)
+    function dispersion(k, l) result(omega)
         real :: k, l, omega
 
         omega = -beta * k/(k**2 + l**2 + Rd**(-2))
  
-    end function dispersion_func
+    end function dispersion
 end module rossby_wave_attributes
