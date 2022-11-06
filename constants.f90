ad1542f85ede8ed9f9c59cb6b09b53deb7078c3a
Vanessa Madu <vanessamadu197@gmail.com>
2022-11-06 16:26:09 +0000

    constants module finished

---
+++ constants.f90
@@ -0,0 +1,8 @@
+module constants
+    implicit none
+    !Constants are non-dimensional with non-dimensionalisation L = 4e5 m and T = 4e6 s
+    real :: pi = 4*atan(1.)
+    real :: g = 3.92e8
+    real :: R_d = 0.1 
+    real :: beta = 32
+end module constants
