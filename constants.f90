686cd8a8dfa08bf53f20d88f139252bfe5b197c4
Vanessa Madu <vanessamadu197@gmail.com>
2022-11-06 16:28:15 +0000

    R_d --> Rd

--- constants.f90
+++ constants.f90
@@ -3,6 +3,6 @@ module constants
     !Constants are non-dimensional with non-dimensionalisation L = 4e5 m and T = 4e6 s
     real :: pi = 4*atan(1.)
     real :: g = 3.92e8
-    real :: R_d = 0.1 
+    real :: Rd = 0.1 
     real :: beta = 32
 end module constants
